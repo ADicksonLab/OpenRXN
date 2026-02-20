60cb993d7576a65342f6b6a6c8bfe103f038404d
Ceren Kilinc <kilincce@msu.edu>
2026-02-19 21:52:03 -0500

    Minor function name fix

--- src/openrxn/compartments/ID.py
+++ src/openrxn/compartments/ID.py
@@ -1,16 +1,16 @@
 
-def join_tup(tup):
+def join_tuple(tup):
     return '_'.join([str(a) for a in tup])
 
-def makeID(array_ID,comp_ID):
+def make_ID(array_ID,comp_ID):
     if array_ID is not None:
         tag = array_ID + '-'
     else:
         tag = ""
         
-    if type(comp_ID) is tuple or type(comp_ID) is list:
-        return tag + join_tup(comp_ID)
-    elif type(comp_ID) is str:
+    if isinstance(comp_ID, tuple) or isinstance(comp_ID, list):
+        return tag + join_tuple(comp_ID)
+    elif isinstance(comp_ID, str):
         return tag + comp_ID
-    elif type(comp_ID) is int:
+    elif isinstance(comp_ID, int):
         return tag + str(comp_ID)
